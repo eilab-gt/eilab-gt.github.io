83dc6aad536f201ee87e4f0fb4337af11ed9cd59
markriedl <riedl@cc.gatech.edu>
2021-05-21 20:58:29 -0400

    updated pubs, fixed bug in bash script

--- bibs/bib2yaml.sh
+++ bibs/bib2yaml.sh
@@ -6,5 +6,5 @@ python json2yaml.py temp1.json temp2.yml
 echo "Fixing yml formatting"
 python fix.py temp2.yml temp3.yml
 echo "Adding bib back into yml"
-python fold.py temp3.yml $2
+python fold.py lab.bib temp3.yml $2
 echo "Copy into _data directory."
